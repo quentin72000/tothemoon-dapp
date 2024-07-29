7ecdf46bc48a3c5f4d81f372b4cf2d86dce01225
quentin72000 <quentin72000@yahoo.com>
2024-07-29 14:32:15 +0200

    Refactor SwapQuantityInput component and update Footer styles

--- client/src/components/layouts/Footer.jsx
+++ client/src/components/layouts/Footer.jsx
@@ -4,7 +4,13 @@ import styles from "../../styles/Footer.module.css";
 export default function Footer() {
   return (
     <footer className={styles.footer}>
+      <div className="">
         <p>Made with ❤️ by <a href="https://github.com/quentin72000" target="_blank">quentin72000</a>.</p>
+      </div>
+      <div>
+      <p>This DApp is <a href="https://github.com/quentin72000/tothemoon-dapp" target="_blank">Open Source</a> and run on the <a href="https://ternoa.network" target="_blank">Ternoa L2 zkEVM+ Testnet</a></p>
+      </div>
+        
     </footer>
   )
 }
